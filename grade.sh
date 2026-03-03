4767dd3488a32fffa136d702e3d57b8ff6e9d5ec
Bobbyyf <rfuller8328@sdsu.edu>
2026-03-03 12:57:34 -0800

    Added grade.sh, the core grading logic (hopefully, if it works). Also added a base test soultion just to see if it works

--- grade.sh
+++ grade.sh
@@ -2,6 +2,23 @@
 
 for i in solutions/*/
 do
-	ls $i
+    problem=$(basename $i)
+    for test_input in tests/$problem/input*.txt
+    do
+        # Extract the test number from the filename (e.g., "input1.txt" -> "1")
+        test_num=$(basename $test_input | grep -o '[0-9]\+')
+        
+        # Run the solution and capture output
+        result=$(python3 solutions/$problem/solution.py < $test_input)
+        
+        # Read the expected output
+        expected=$(cat tests/$problem/expected$test_num.txt)
+        
+        # Compare them
+        if [ "$result" = "$expected" ]; then
+            echo "$problem test $test_num: PASS"
+        else
+            echo "$problem test $test_num: FAIL"
+        fi
+    done
 done
-
